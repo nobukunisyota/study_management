23f2d8faf8cc68b732668ac304a8ac89df4dc1ff
nobukunisyota <nobukuni.syota0727@gmail.com>
2024-01-09 23:38:07 +0900

    Add Input function

--- main.rs
+++ main.rs
@@ -1,10 +1,20 @@
 use std::io;
 
 fn main() {
-    println!("please input title !");
+    loop {
+        println!("please input title !");
+        let title = input_title();
+        if title.trim() == "exit" {
+            break;
+        }
+        println!("title is {}", title);
+    }
+}
+
+fn input_title() -> String {
     let mut title = String::new();
     io::stdin()
         .read_line(&mut title)
         .expect("Failed to read line");
-
+    title
 }
