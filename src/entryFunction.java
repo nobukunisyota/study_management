4fa3cf54a3cd1536b6543fed7999650c5e0a8bb7
nobukunisyota <nobukuni.syota0727@gmail.com>
2024-06-24 23:57:08 +0900

    Add Tasks Class

--- src/entryFunction.java
+++ src/entryFunction.java
@@ -1,5 +1,13 @@
+/**
+ * Tasks Class
+ */
+public class Tasks {
+    String name;
+
+}
+
 public class entryFunction {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
     }
 }
